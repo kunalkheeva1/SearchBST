afa49d492860fdd4d436b9c31f0356eecd1133c7
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-21 21:20:10 -0700

    Initial commit

---
+++ src/SearchBST.java
@@ -0,0 +1,20 @@
+public class SearchBST {
+
+    class Node{
+        int val;
+        Node left;
+        Node right;
+        Node(int val, Node left, Node right){
+         this.val = val;
+         this.left = left;
+         this.right = right;
+        }
+    }
+    
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
